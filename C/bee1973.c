8f40032ce4b94c3f75c252da9ca6e2133535fdd6
elvis-martins <elvismarttins@hotmail.com>
2022-09-26 16:32:26 -0300

    Bee 1973 em C - Nível 7

---
+++ C/bee1973.c
@@ -0,0 +1,36 @@
+//Nível 7
+#include<stdio.h>
+
+int main(){
+    int N;
+    unsigned long long int indice_estrela = 0, total = 0, qtd_estrelas_atacadas=0;
+
+    scanf("%d",&N);
+    int estrela[N], estrela_roubada[N];
+
+    for(int i=0; i <N; i++){
+        scanf("%d", &estrela[i]);
+        total+= estrela[i];
+        estrela_roubada[i] = -1;
+    }
+
+    while(indice_estrela >= 0  && indice_estrela < N){
+        if(estrela_roubada[indice_estrela] != indice_estrela){
+            estrela_roubada[indice_estrela] = indice_estrela;
+            qtd_estrelas_atacadas++;
+        }
+        
+        if(estrela[indice_estrela] > 0){
+            estrela[indice_estrela]--;
+            total--;
+            ((estrela[indice_estrela] + 1) % 2 == 0) ? indice_estrela-- : indice_estrela++;
+        }
+        else {
+            indice_estrela --;
+        }
+    }
+
+    printf("%llu %llu\n", qtd_estrelas_atacadas, total);
+
+    return 0;
+}
